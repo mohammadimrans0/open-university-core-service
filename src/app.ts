7a72d501b7c131322dafa6705f43a0abdde59869
imrans09 <mohammadimrans09t@gmail.com>
2023-05-30 21:22:23 +0600

    [done] setup express mongoose cors

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+const app: Application = express();
+
+app.use(cors());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// testing
+app.get('/', (req, res) => {
+    res.send('working successfully');
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function bootstrap() {
+    try {
+        await mongoose.connect(config.database_url as string);
+        console.log('Database is connected successfully');
+
+        app.listen(config.port, () => {
+            console.log(`app is listening on port ${config.port}`);
+        });
+    } catch (error) {
+        console.log('Failed to connect database', error);
+    }
+}
+
+bootstrap();
