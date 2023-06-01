aa4af696d713b73e10ec18cb0c7daa473f3616ad
imrans09 <mohammadimrans09t@gmail.com>
2023-06-01 21:20:41 +0600

    [done] UM-7 task

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import express, { Application } from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
+import userRouter from '../src/app/modules/users/users.route'
 
 app.use(cors())
 
@@ -8,8 +9,12 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// Application routes
+
+app.use('/api/v1/users/', userRouter)
+
 // testing
-app.get('/', (req, res) => {
+app.get('/', async (req: Request, res: Response) => {
   res.send('working successfully')
 })
 
---
+++ src/app/modules/users/users.service.ts
@@ -0,0 +1,24 @@
+import config from '../../../config/index'
+import { IUser } from './users.interface'
+import { User } from './users.model'
+import { generateUserId } from './users.utils'
+
+const createUser = async (user: IUser): Promise<IUser | null> => {
+  // auto generated incremental id
+  const id = await generateUserId()
+  user.id = id
+  // default password
+  if (!user.password) {
+    user.password = config.default_user_password as string
+  }
+
+  const createUser = await User.create(user)
+  if (!createUser) {
+    throw new Error('Failed to create user')
+  }
+  return createUser
+}
+
+export default {
+  createUser,
+}
